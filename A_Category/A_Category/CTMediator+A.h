d7e77bcb4f940e36a7c32fc7e78ba716417d6fe0
wangjian <wangjian@qhfax.com.cn>
2017-04-26 10:33:09 +0800

    添加依赖

---
+++ A_Category/A_Category/CTMediator+A.h
@@ -0,0 +1,13 @@
+//
+//  CTMediator+A.h
+//  A_Category
+//
+//  Created by wangjian on 2017/4/26.
+//  Copyright © 2017年 com.qhfax. All rights reserved.
+//
+
+#import <CTMediator/CTMediator.h>
+
+@interface CTMediator (A)
+
+@end
---
+++ A_Category/A_Category/CTMediator+A.m
@@ -0,0 +1,13 @@
+//
+//  CTMediator+A.m
+//  A_Category
+//
+//  Created by wangjian on 2017/4/26.
+//  Copyright © 2017年 com.qhfax. All rights reserved.
+//
+
+#import "CTMediator+A.h"
+
+@implementation CTMediator (A)
+
+@end
